a202aeb3cee0414be23dfa9cc7a02526ec7a68b6
siyamulislam <siyamulislam1@gmail.com>
2024-10-10 23:52:50 +0600

    ts smartness declear without assign

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 const greeting = 'Hello, TypeScript!';
 console.log(greeting);
+let num;
+num = 'siam';
+num = 45;
+console.log(num);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 const greeting: string = 'Hello, TypeScript!';
 console.log(greeting); 
+let num 
+num = 'siam'
+num= 45
+console.log(num);
