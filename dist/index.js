5af11e1c263f8c139382f00e0c431105a7e31687
siyamulislam <siyamulislam1@gmail.com>
2024-10-11 00:23:19 +0600

    Explicit typeof with union

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,48 @@
 "use strict";
 const greeting = 'Hello, TypeScript!';
 console.log(greeting);
+// Explicit typeof variable 
+let age;
+age = 24;
+console.log(age);
+//  with union
+let age2;
+age2 = 24;
+age2 = 'siam';
+console.log(age2);
+// Explicit typeof array 
+let x = [];
+x.push('siam');
+x.push('siam2');
+console.log(x);
+// with union
+let y = [];
+y.push('siam');
+y.push(24);
+console.log(y);
 // object
-let person = {
-    name: "Siam",
-    age: 25,
-    premium: true
+let c;
+c = {
+    name: '$toma',
+    age: 27
 };
-person.age = 26;
-console.log(person);
-// array
+console.log(c);
+let b;
+b = [1, 2, 3, 4, 5, 6]; //array is a type of object in js
+console.log(b);
+// // object
+// let person = {
+//     name: "Siam",
+//     age:25,
+//     premium:true
+// }
+// person.age=26
+// console.log(person);
+// // array
 // let fruit =['apple',5,'banana',false]
 // fruit.push(true)
 // console.log(fruit);
-//function
+// //function
 // function multiply(a:number,b:number){
 //     return a+b;
 // }
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,72 @@
 const greeting: string = 'Hello, TypeScript!';
-console.log(greeting); 
+console.log(greeting);
+
+
+// Explicit typeof variable 
+let age:number 
+age=24
+console.log(age);
+
+//  with union
+let age2:(number|string)
+age2=24
+age2 ='siam'
+console.log(age2);
+
+ // Explicit typeof array 
+let x:string[]=[];
+x.push('siam')
+x.push('siam2')
+console.log(x);
+
+// with union
+let y:(string| number)[]=[];
+y.push('siam')
+y.push(24)
+console.log(y);
+
 
 // object
 
-let person = {
-    name: "Siam",
-    age:25,
-    premium:true
+let c: {
+    name: string,
+    age: number
+};
+c = {
+    name: '$toma',
+    age: 27
 }
-person.age=26
-console.log(person);
+console.log(c);
+
+let b: object;
+b = [1, 2, 3, 4, 5, 6] //array is a type of object in js
+console.log(b);
 
 
 
 
-// array
+
+
+
+// // object
+
+// let person = {
+//     name: "Siam",
+//     age:25,
+//     premium:true
+// }
+// person.age=26
+// console.log(person);
+
+
+// // array
 
 // let fruit =['apple',5,'banana',false]
 // fruit.push(true)
 // console.log(fruit);
 
 
-//function
+// //function
 
 // function multiply(a:number,b:number){
 //     return a+b;
