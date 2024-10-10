04909f1c250edcf86419c3f3fec98f2a2b2136a1
siyamulislam <siyamulislam1@gmail.com>
2024-10-11 00:02:36 +0600

    object

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 const greeting: string = 'Hello, TypeScript!';
 console.log(greeting); 
 
+// object
+
+let person = {
+    name: "Siam",
+    age:25,
+    premium:true
+}
+person.age=26
+console.log(person);
+
+
+
+
 // array
-let fruit =['apple',5,'banana',false]
-fruit.push(true)
-console.log(fruit);
 
-
+// let fruit =['apple',5,'banana',false]
+// fruit.push(true)
+// console.log(fruit);
 
 
 //function
+
 // function multiply(a:number,b:number){
 //     return a+b;
 // }
