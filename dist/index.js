ec7b7613c541a0709561ef41324a9e33b5324d6e
siyamulislam <siyamulislam1@gmail.com>
2024-10-10 23:58:37 +0600

    array - push with diffrent data

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,12 @@
 "use strict";
 const greeting = 'Hello, TypeScript!';
 console.log(greeting);
+// array
+let fruit = ['apple', 5, 'banana', false];
+fruit.push(true);
+console.log(fruit);
 //function
-function multiply(a, b) {
-    return a + b;
-}
-console.log(multiply(4, 5));
+// function multiply(a:number,b:number){
+//     return a+b;
+// }
+// console.log(multiply(4,5));
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 const greeting: string = 'Hello, TypeScript!';
 console.log(greeting); 
 
+// array
+let fruit =['apple',5,'banana',false]
+fruit.push(true)
+console.log(fruit);
+
 
 
 
 //function
-function multiply(a:number,b:number){
-    return a+b;
-}
-console.log(multiply(4,5));
+// function multiply(a:number,b:number){
+//     return a+b;
+// }
+// console.log(multiply(4,5));
